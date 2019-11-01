639cad4c2a1976cf1346c79819c65298f27597a2
fsbalbuena <fsbalbuena@gmail.com>
2019-11-01 12:07:23 -0300

    add server and public folders

---
+++ server/routes/rates.js
@@ -0,0 +1,18 @@
+const router=require("express").Router()
+
+router.get('/', (req, res, next) => {
+ 
+});
+
+router.post('/', (req, res, next) => {
+ 
+});
+
+router.put('/:id', (req, res, next) => {
+ 
+});
+
+router.delete('/',(req,res,next)=>{
+
+})
+module.exports=router
