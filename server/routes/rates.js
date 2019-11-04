a06f3f69a33a374289741cc57dcc02ba34562eac
fsbalbuena <fsbalbuena@gmail.com>
2019-11-04 12:56:39 -0300

    Only Fetching the api on api/rates

--- server/routes/rates.js
+++ server/routes/rates.js
@@ -1,18 +1,22 @@
 const router=require("express").Router()
+const axios =require("axios")
+
+const symbols=["EUR","USD","ARS","BRL"]
+
 
 router.get('/', (req, res, next) => {
- 
+    res.status(200).send({symbols}) 
 });
+
+router.get("/:id", (req,res,next)=>{
+    
+})
 
 router.post('/', (req, res, next) => {
- 
+   
 });
 
-router.put('/:id', (req, res, next) => {
- 
+router.post('/custom-rate', (req, res, next) => {
+    
 });
-
-router.delete('/',(req,res,next)=>{
-
-})
 module.exports=router
