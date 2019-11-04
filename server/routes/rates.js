4a72dc15a6542d20bd97e397c8f77d677935749b
fsbalbuena <fsbalbuena@gmail.com>
2019-11-04 13:08:51 -0300

    handle the rates for different bases

--- server/routes/rates.js
+++ server/routes/rates.js
@@ -1,15 +1,31 @@
 const router=require("express").Router()
 const axios =require("axios")
+const {symbols,
+    notAvailable,
+    ratesFromEurToBase} = require('./utils')
 
-const symbols=["EUR","USD","ARS","BRL"]
-
+const catchError=err=>{
+    res.status(err.status || 500)
+    .send({message:err.message || "Something went wrong"})
+}
 
 router.get('/', (req, res, next) => {
     res.status(200).send({symbols}) 
 });
 
 router.get("/:id", (req,res,next)=>{
+    let base=req.params.id.toUpperCase()
+    //check for symbol available
+    if(notAvailable(base)){
+        res.status(404).send({message:"Not available currency"})
+    }
     
+    axios
+    .get(`http://data.fixer.io/api/latest?symbols=${symbols.join()}&access_key=${process.env.API_KEY}`)
+    .then(api=>api.data)
+    .then(apiData=> ratesFromEurToBase(apiData,base))
+    .then(pairs=>res.send({pairs}))
+    .catch(catchError)
 })
 
 router.post('/', (req, res, next) => {
--- server/routes/utils.js
+++ server/routes/utils.js
@@ -3,7 +3,40 @@ const symbols=["EUR","USD","ARS","BRL"]
 
 const notAvailable=(currency)=>!symbols.includes(currency)
 
+const fix=(num,fixed=5)=>Number(num.toFixed(fixed))
+
+const divide=(firstNum,SecondNum)=>fix(firstNum/SecondNum)
+
+const makePairName=(base,destination)=>`${base}/${destination}`
+
+const makePairOfRates=(ratesOnEur,base,destination)=>{
+        let direct={
+            name:makePairName(base,destination),
+            rate:divide(ratesOnEur[destination],ratesOnEur[base])
+        }
+        let reverse={
+            name:makePairName(destination,base),
+            rate:divide(ratesOnEur[base],ratesOnEur[destination])
+        }
+        return [direct,reverse]
+    }
+
+const ratesBeetween=(ratesOnEur,base,destination)=>({
+    name:`${base} - ${destination}`,
+    rates:makePairOfRates(ratesOnEur,base,destination)
+    })
+
+const ratesFromEurToBase=({rates},base)=>{
+    let values=[]
+    for(let key in rates){
+        if(key!==base){
+            values.push(ratesBeetween(rates,base,key))
+        }
+    }
+    return values
+}
 module.exports = {
     symbols,
-    notAvailable
+    notAvailable,
+    ratesFromEurToBase
 }
