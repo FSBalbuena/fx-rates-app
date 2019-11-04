807a45529aa937ae70a4c16a94eec6ca5a9d7f85
fsbalbuena <fsbalbuena@gmail.com>
2019-11-04 14:27:36 -0300

    rates route calls for symbols

--- src/react/containers/RatesContainer.jsx
+++ src/react/containers/RatesContainer.jsx
@@ -1,18 +1,35 @@
 import React,{useEffect} from 'react';
 import { connect } from 'react-redux';
-
+import {fetchSymbols} from '../../redux/actions/symbols-actions'
 import RatesComponent from '../components/Rates'
 
-const ExampleContainer= ({isDesktop})=>{
-    
-    return <RatesComponent isDesktop={isDesktop}/>
+const ExampleContainer= ({fetchSymbols,isDesktop,symbols})=>{
+    useEffect(
+        ()=>{
+            symbols.data.length || fetchSymbols()
+        }
+    )
+    const data={
+        symbols:symbols.data,
+        isDesktop,
+    }
+
+    return symbols.loading?
+    <h1>loading...</h1>
+    :
+    symbols.error?
+    <h1>{symbols.error}</h1>
+    :
+    <RatesComponent {...data}/>
 }
 
 /* react-redux settings */
 const mapStateToProps=(state,ownProps)=>({
-    isDesktop:state.ui.isDesktop
+    isDesktop:state.ui.isDesktop,
+    symbols:state.symbols
 })
 const mapDispatchToProps=(dispatch,ownProps)=>({
+    fetchSymbols:()=>dispatch(fetchSymbols())
 })
 
 export default connect(mapStateToProps,mapDispatchToProps)(ExampleContainer)
